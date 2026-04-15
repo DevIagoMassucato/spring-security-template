01c74659ea770605ebf6deb97b98daa58fe95171
Dev Iago Massucato <dev.iagomassucato@gmail.com>
2026-04-15 11:10:23 -0300

    feat: add authentication for specific HTTP methods

--- src/main/java/com/iagomassucato/spring/security/template/config/SecurityConfig.java
+++ src/main/java/com/iagomassucato/spring/security/template/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.iagomassucato.spring.security.template.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,19 +10,27 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http
-                .csrf().disable() // necessário para API
-                .authorizeRequests() //autorizar todas
-                .anyRequest().authenticated() // qualquer requisição autenticado - tudo precisa de login
+                .csrf().disable()
+                .authorizeRequests()// //autorizar todas
+                .antMatchers(HttpMethod.POST, "/api/v1/anime").hasRole("ADMIN")// aplica autenticação nesse método http, precisa do role admin
+                .antMatchers(HttpMethod.GET, "/api/v1/anime").authenticated()// aplica autenticação, qualquer usuário autenticado
+                .anyRequest().denyAll()
                 .and()
-                .httpBasic(); // usa Basic Auth
+                .httpBasic();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
         auth.inMemoryAuthentication()
                 .withUser("admin")
-                .password("{noop}123") // {noop} não faça nenhum tipo de criptografia na senha - Password Encoder
+                .password("{noop}123")
+                .roles("ADMIN")
+                .and()
+                .withUser("user")
+                .password("{noop}123")
                 .roles("USER");
     }
 
