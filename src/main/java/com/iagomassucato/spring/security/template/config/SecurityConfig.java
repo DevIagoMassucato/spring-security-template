c3194f713aafda0e6463f2353f78f4f595a48170
Dev Iago Massucato <dev.iagomassucato@gmail.com>
2026-05-14 12:48:01 -0300

    feat(security): add role-based access control in security configuration layer

--- src/main/java/com/iagomassucato/spring/security/template/config/SecurityConfig.java
+++ src/main/java/com/iagomassucato/spring/security/template/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.iagomassucato.spring.security.template.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,18 +11,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable() // necessário para API
-                .authorizeRequests() // autorizar todos os endpoint
-                .anyRequest().authenticated() // qualquer requisição autenticado
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/api/v1/animes").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET, "/api/v1/animes/**").hasRole("USER")
+                .antMatchers(HttpMethod.PATCH, "/api/v1/animes/**").hasAnyRole("ADMIN", "USER")
+                .antMatchers(HttpMethod.DELETE, "/api/v1/animes/**").hasRole("ADMIN")
+                .anyRequest().denyAll()
                 .and()
-                .httpBasic(); // usa Basic Auth
+                .httpBasic();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
         auth.inMemoryAuthentication()
                 .withUser("admin")
-                .password("{noop}123") // {noop} não faça nenhum tipo de criptografia na senha - Password Encoder
+                .password("{noop}123")
+                .roles("ADMIN")
+                .and()
+                .withUser("user")
+                .password("{noop}123")
                 .roles("USER");
     }
 
