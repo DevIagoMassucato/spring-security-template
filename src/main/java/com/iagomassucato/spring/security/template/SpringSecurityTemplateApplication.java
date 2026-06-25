3d92ac0736e0bf3c85ef801d9f9dc7e94dea1012
Dev Iago Massucato <dev.iagomassucato@gmail.com>
2026-06-25 10:09:16 -0300

    chore: init project (java 11, spring boot 2.1.3.RELEASE)

---
+++ src/main/java/com/iagomassucato/spring/security/template/SpringSecurityTemplateApplication.java
@@ -0,0 +1,11 @@
+package com.iagomassucato.spring.security.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityTemplateApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityTemplateApplication.class, args);
+    }
+}
