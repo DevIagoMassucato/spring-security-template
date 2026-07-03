cc01a5ff763f3802b793efece38d2381b5cbc42e
Dev Iago Massucato <dev.iagomassucato@gmail.com>
2026-07-03 15:21:07 -0300

    feat(security): add database-backed user authentication

--- src/main/java/com/iagomassucato/spring/security/template/SpringSecurityTemplateApplication.java
+++ src/main/java/com/iagomassucato/spring/security/template/SpringSecurityTemplateApplication.java
@@ -2,10 +2,13 @@ package com.iagomassucato.spring.security.template;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class SpringSecurityTemplateApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityTemplateApplication.class, args);
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        System.out.println("PASSWORD => "+bCryptPasswordEncoder.encode("123"));
     }
 }
