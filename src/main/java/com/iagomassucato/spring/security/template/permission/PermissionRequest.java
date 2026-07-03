18ae95f529aaf3f0348bb26bc3f06a5062ca3bb3
Dev Iago Massucato <dev.iagomassucato@gmail.com>
2026-07-03 16:22:02 -0300

    feat(permission): add permission request and response dtos

---
+++ src/main/java/com/iagomassucato/spring/security/template/permission/PermissionRequest.java
@@ -0,0 +1,10 @@
+package com.iagomassucato.spring.security.template.permission;
+
+import lombok.Getter;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+public class PermissionRequest {
+    @NotBlank(message = "name is required")
+    private String name;
+}
---
+++ src/main/java/com/iagomassucato/spring/security/template/permission/PermissionResponse.java
@@ -0,0 +1,18 @@
+package com.iagomassucato.spring.security.template.permission;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class PermissionResponse {
+    private Long id;
+    private String name;
+
+    public static PermissionResponse fromEntity (PermissionEntity permissionEntity){
+        return new PermissionResponse(
+                permissionEntity.getId(),
+                permissionEntity.getName()
+        );
+    }
+}
