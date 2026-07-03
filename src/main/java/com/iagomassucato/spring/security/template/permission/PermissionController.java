a4fcb740487cb545a7976407ab212afecf21136d
Dev Iago Massucato <dev.iagomassucato@gmail.com>
2026-07-03 16:47:22 -0300

    feat(permission): add authorization to permission controller endpoints

--- src/main/java/com/iagomassucato/spring/security/template/permission/PermissionController.java
+++ src/main/java/com/iagomassucato/spring/security/template/permission/PermissionController.java
@@ -3,6 +3,7 @@ package com.iagomassucato.spring.security.template.permission;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
@@ -14,6 +15,7 @@ public class PermissionController {
 
     private final PermissionService permissionService;
 
+    @PreAuthorize("hasRole('TI')")
     @PostMapping
     public ResponseEntity<PermissionResponse> create(@Valid @RequestBody PermissionRequest permissionRequest){
         PermissionResponse permissionResponse = permissionService.create(permissionRequest);
@@ -22,6 +24,7 @@ public class PermissionController {
                 .body(permissionResponse);
     }
 
+    @PreAuthorize("hasRole('TI')")
     @PutMapping("/{id}")
     public ResponseEntity<PermissionResponse> replace(
             @PathVariable Long id, @Valid @RequestBody PermissionRequest permissionRequest){
@@ -29,18 +32,21 @@ public class PermissionController {
         return ResponseEntity.ok(permissionResponse);
     }
 
+    @PreAuthorize("hasRole('TI')")
     @GetMapping
     public ResponseEntity<List<PermissionResponse>> findAll(){
         List<PermissionResponse> permissionResponseList = permissionService.findAll();
         return ResponseEntity.ok(permissionResponseList);
     }
 
+    @PreAuthorize("hasRole('TI')")
     @GetMapping("/{id}")
     public ResponseEntity<PermissionResponse> findById(@PathVariable Long id){
         PermissionResponse permissionResponse = permissionService.findById(id);
         return ResponseEntity.ok(permissionResponse);
     }
 
+    @PreAuthorize("hasRole('TI')")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         permissionService.delete(id);
