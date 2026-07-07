8adb06578d1d1b716c79e717e086b1e3691157ac
Dev Iago Massucato <dev.iagomassucato@gmail.com>
2026-07-07 10:07:01 -0300

    feat(auth): add authentication service with jwt token generation

---
+++ src/main/java/com/iagomassucato/spring/security/template/security/auth/AuthService.java
@@ -0,0 +1,31 @@
+package com.iagomassucato.spring.security.template.security.auth;
+
+import com.iagomassucato.spring.security.template.security.jwt.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    public AuthResponse login(AuthRequest authRequest) {
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                authRequest.getUsername(),
+                authRequest.getPassword()
+        );
+
+        authenticationManager.authenticate(authenticationToken);
+        UserDetails userDetails = userDetailsService.loadUserByUsername(authRequest.getUsername());
+        String token = jwtService.generateToken(userDetails);
+        return new AuthResponse(token);
+    }
+}
