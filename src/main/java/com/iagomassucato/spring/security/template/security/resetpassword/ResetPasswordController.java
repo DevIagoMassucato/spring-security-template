7bcff9ffbc0d408d065be055d40fbf5624b69fd4
Dev Iago Massucato <dev.iagomassucato@gmail.com>
2026-08-07 10:22:17 -0300

    feat(resetpassword): add reset password controller with forgot-password and reset-password endpoints

---
+++ src/main/java/com/iagomassucato/spring/security/template/security/resetpassword/ResetPasswordController.java
@@ -0,0 +1,26 @@
+package com.iagomassucato.spring.security.template.security.resetpassword;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v1/auth/forgot-password")
+@RequiredArgsConstructor
+public class ResetPasswordController {
+
+    private final ResetPasswordService resetPasswordService;
+
+    @PostMapping
+    public ResponseEntity<ResetPasswordResponse> forgotPassword(@Valid @RequestBody ResetPasswordRequest resetPasswordRequest) {
+        ResetPasswordResponse response = resetPasswordService.forgotPassword(resetPasswordRequest);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/reset")
+    public ResponseEntity<Void> resetPassword(@Valid @RequestBody ConfirmPasswordResetRequest request) {
+        resetPasswordService.resetPassword(request);
+        return ResponseEntity.noContent().build();
+    }
+}
