f6d4f1d03a3989be4c76eb4f92868c58991c06b7
Dev Iago Massucato <dev.iagomassucato@gmail.com>
2026-08-21 12:18:40 -0300

    feat(oauth2): update oauth2 login to use sessions

--- src/main/java/com/iagomassucato/spring/security/template/security/oauth2/OAuth2SuccessHandler.java
+++ src/main/java/com/iagomassucato/spring/security/template/security/oauth2/OAuth2SuccessHandler.java
@@ -4,7 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.iagomassucato.spring.security.template.security.auth.AuthResponse;
 import com.iagomassucato.spring.security.template.security.jwt.JwtService;
 import com.iagomassucato.spring.security.template.security.jwt.JwtToken;
-import com.iagomassucato.spring.security.template.security.refreshtoken.RefreshTokenService;
+import com.iagomassucato.spring.security.template.security.refreshtoken.RefreshTokenCreator;
+import com.iagomassucato.spring.security.template.security.session.SessionCreator;
+import com.iagomassucato.spring.security.template.security.session.SessionEntity;
 import com.iagomassucato.spring.security.template.security.userdetails.UserDetailsImpl;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -19,8 +21,9 @@ import java.io.IOException;
 public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
     private final OAuth2AuthenticationService oAuth2AuthenticationService;
+    private final SessionCreator sessionCreator;
     private final JwtService jwtService;
-    private final RefreshTokenService refreshTokenService;
+    private final RefreshTokenCreator refreshTokenCreator;
     private final ObjectMapper objectMapper;
 
     @Override
@@ -29,11 +32,17 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
             HttpServletResponse response,
             Authentication authentication
     ) throws IOException {
+
         UserDetailsImpl userDetailsImpl = oAuth2AuthenticationService.authenticate(authentication);
-        JwtToken accessToken = jwtService.generateAccessToken(userDetailsImpl.getUserEntity());
-        JwtToken refreshToken = jwtService.generateRefreshToken(userDetailsImpl.getUserEntity());
-        refreshTokenService.create(refreshToken, userDetailsImpl.getUserEntity());
-        AuthResponse authResponse = new AuthResponse(accessToken.getToken(), refreshToken.getToken());
+        String ipAddress = request.getRemoteAddr();
+        String userAgent = request.getHeader("User-Agent");
+        SessionEntity sessionEntity = sessionCreator.create(userDetailsImpl.getUserEntity(), ipAddress, userAgent);
+        Long userId = userDetailsImpl.getUserEntity().getId();
+        Long sessionId = sessionEntity.getId();
+        JwtToken accessToken = jwtService.generateAccessToken(userId, sessionId);
+        JwtToken refreshToken = jwtService.generateRefreshToken(userId, sessionId);
+        refreshTokenCreator.create(refreshToken, sessionEntity);
+        AuthResponse authResponse = new AuthResponse(accessToken.token(), refreshToken.token());
         response.setContentType("application/json");
         response.setStatus(HttpServletResponse.SC_OK);
         objectMapper.writeValue(response.getOutputStream(), authResponse);
